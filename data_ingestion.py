9497a38c6488147efb2fb27438922edfc0ca86e3
Manisha Goyal <manisha.goyal1996@gmail.com>
2024-11-28 20:05:40 -0500

    added code to create index if it does not exist

--- data_ingestion.py
+++ data_ingestion.py
@@ -5,8 +5,10 @@ from langchain_community.document_loaders import PyPDFLoader, DirectoryLoader
 from langchain_text_splitters import CharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings
 from langchain_pinecone import PineconeVectorStore
+from pinecone import Pinecone, ServerlessSpec
 
 openai_api_key = Config.OPENAI_API_KEY
+pinecone_api_key = Config.PINECONE_API_KEY
 index_name = Config.INDEX_NAME
 
 # Configure logging
@@ -15,6 +17,25 @@ logger = logging.getLogger(__name__)
 
 def get_vectorstore(embeddings, index_name):
     """Get or create a Pinecone vectorstore"""
+
+    # Initialize Pinecone client
+    pc = Pinecone(api_key=os.getenv("PINECONE_API_KEY"), environment='us-east-1')
+
+    # Check if the index exists and create if it does not
+    if index_name not in pc.list_indexes().names():
+        pc.create_index(
+            name=index_name,
+            dimension=1536,
+            metric="cosine",
+            spec=ServerlessSpec(
+                cloud="aws",
+                region="us-east-1",
+            )
+        )
+        logger.info(f"Created index: {index_name}")
+    else:
+        logger.info(f"Index {index_name} already exists.")
+
     return PineconeVectorStore(
         index_name=index_name,
         embedding=embeddings
@@ -65,7 +86,7 @@ def initial_ingestion():
     logger.info("Starting initial ingestion...")
 
     # Initialize embeddings and vectorstore
-    embeddings = OpenAIEmbeddings(openai_api_type=openai_api_key)
+    embeddings = OpenAIEmbeddings(model="text-embedding-3-small", openai_api_key=openai_api_key)
     vectorstore = get_vectorstore(embeddings, index_name)
     
     # Load the PDFs and add to vectorstore
